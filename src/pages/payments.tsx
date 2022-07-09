1d56002bbbb3666279bce84b6b0bdd7b36b13106
Giovanna Cunha <giovannakelli@gmail.com>
2022-07-09 14:05:09 -0300

    feat: add credit card

--- src/pages/payments.tsx
+++ src/pages/payments.tsx
@@ -1,7 +1,13 @@
-import { Box, Card, CardContent, FormLabel, Grid } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardContent,
+  FormLabel,
+  Grid,
+  Typography,
+} from '@mui/material';
 import {
   CardCvcElement,
-  CardElement,
   CardExpiryElement,
   CardNumberElement,
 } from '@stripe/react-stripe-js';
@@ -9,8 +15,10 @@ import React from 'react';
 import { Layout } from '../components/stateless/Layout';
 import { PaperComponent } from '../components/stateless/PaperComponent';
 import { TopBar } from '../components/stateless/TopBar';
+import { Controller, useForm } from 'react-hook-form';
 import {
   ButtonPayment,
+  CreditCard,
   InputPayment,
   LabelPayment,
   TitlePayment,
@@ -26,6 +34,14 @@ const options = {
 };
 
 const Payments = () => {
+  const {
+    handleSubmit,
+    register,
+    setValue,
+    getValues,
+    control,
+    formState: { errors },
+  } = useForm();
   return (
     <PaperComponent>
       <TopBar />
@@ -34,7 +50,13 @@ const Payments = () => {
           <Grid container spacing={3} justifyContent="center">
             <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
               <Card elevation={0}>
-                <CardContent sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <CardContent
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
                   <form>
                     <Grid container spacing={3}>
                       <Grid item xs={12} sm={12} md={12} lg={12}>
@@ -65,7 +87,61 @@ const Payments = () => {
               </Card>
             </Grid>
             <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-              
+              <Box
+                mt={5}
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  minHeight: '40vh',
+                }}
+              >
+                <CreditCard>
+                  <CardContent>
+                    <Grid container>
+                      <Grid item xs={12} sm={12} md={12}>
+                        <img
+                          src="/chip.png"
+                          alt="card chip"
+                          style={{ maxWidth: '40px' }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={12} md={12}>
+                        <Typography
+                          sx={{
+                            color: 'white',
+                            textAlign: 'center',
+                            paddingTop: '3rem',
+                          }}
+                        >
+                          4242 4242 4242 4242
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={12} sm={12} md={12}>
+                        <Typography
+                          sx={{
+                            fontSize: '1rem',
+                            color: 'white',
+                            paddingTop: '2rem',
+                            textAlign: 'end',
+                          }}
+                        >
+                          EXPIRATION
+                        </Typography>
+                        <Typography
+                          sx={{
+                            color: 'white',
+                            fontSize: '0.8rem',
+                            textAlign: 'end',
+                          }}
+                        >
+                          12/28
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                </CreditCard>
+              </Box>
             </Grid>
           </Grid>
         </Box>
--- src/styles/payments/styles.ts
+++ src/styles/payments/styles.ts
@@ -34,3 +34,20 @@ export const InputPayment = styled(Box)`
 `;
 
 export const ButtonPayment = styled(FullWidthButton)``;
+
+export const CreditCard = styled(Card)`
+  min-width: 27rem;
+  height: 15rem;
+  background: #232526; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #414345,
+    #232526
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #414345,
+    #232526
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  border-radius: 6px;
+`;
